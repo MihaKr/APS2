bd35338dce44bad5fda54afecf296720c41df743
MihaKr <75996374+MihaKr@users.noreply.github.com>
2022-04-21 04:54:27 +0200

    2.dn 6/10

---
+++ src/DN2.java
@@ -0,0 +1,243 @@
+class Node {
+    entry key;
+    Node left, right;
+
+    public Node(entry item) {
+        key = item;
+        left = right = null;
+    }
+}
+
+class entry  {
+    int key;
+    int occ;
+
+    public entry(int key, int occ) {
+        this.key = key;
+        this.occ = occ;
+    }
+}
+
+class BST {
+    Node root;
+    int compares = 0;
+    boolean sr = true;
+
+    BST() {
+        root = null;
+    }
+
+    void insert(int key) {
+        if(find_t(key)) {
+        }
+        else {
+            entry e = new entry(key, 1);
+            root = recursion(root, e);
+        }
+    }
+
+    Node recursion(Node root, entry key) {
+        if (root == null) {
+            root = new Node(key);
+            return root;
+        }
+
+        if (key.key < root.key.key)
+            root.left = recursion(root.left, key);
+        else if (key.key > root.key.key)
+            root.right = recursion(root.right, key);
+
+        return root;
+    }
+
+    boolean find_t(int key) {
+        return findRec_t(this.root, key);
+    }
+
+    boolean findRec_t (Node x, int key) {
+        if(x == null) {
+            return false;
+        }
+
+        if(x.key.key == key) {
+            x.key.occ +=1;
+            return true;
+        }
+
+        if(key < x.key.key) {
+            return findRec_t(x.left, key);
+        }
+
+        else {
+            return findRec_t(x.right, key);
+        }
+    }
+
+
+
+    boolean find(int key) {
+        return findRec(this.root, key);
+    }
+
+    boolean findRec (Node x, int key) {
+        this.compares++;
+        if(x == null) {
+            return false;
+        }
+
+        this.compares++;
+        if(x.key.key == key) {
+            return true;
+        }
+
+        this.compares++;
+        if(key < x.key.key) {
+            return findRec(x.left, key);
+        }
+
+        else {
+            return findRec(x.right, key);
+        }
+    }
+
+    void delete(int key) {
+        root = deleteR(root, key);
+    }
+
+
+    Node deleteR(Node root, int key) {
+        if (root == null) {
+            return root;
+        }
+
+        if (key < root.key.key) {
+            root.left = deleteR(root.left, key);
+        }
+        else if (key > root.key.key) {
+            root.right = deleteR(root.right, key);
+        }
+        else {
+            if (root.left == null) {
+                return root.right;
+            }
+            else if (root.right == null) {
+                return root.left;
+            }
+
+            root.key.occ--;
+            if(root.key.occ == 0) {
+                if(sr == true) {
+                    root.key = minValue(root.left);
+                    sr = false;
+                    root.left = deleteR(root.left, root.key.key);
+
+                }
+                else {
+                    root.key = maxValue(root.right);
+                    sr = true;
+                    root.right = deleteR(root.right, root.key.key);
+
+
+                }
+            }
+        }
+
+        return root;
+    }
+
+    entry minValue(Node root) {
+        entry min = root.key;
+        while (root.left != null)
+        {
+            min = root.left.key;
+            root = root.left;
+        }
+        return min;
+    }
+
+    entry maxValue(Node root) {
+        entry max = root.key;
+        while (root.right != null)
+        {
+            max = root.right.key;
+            root = root.right;
+        }
+        return max;
+    }
+
+
+
+    void printInorder() {
+        if(this.root != null) {
+            inRec(this.root);
+        }
+    }
+
+    void printPreorder() {
+        if(this.root != null) {
+            preRec(this.root);
+        }
+    }
+
+    void printPostorder() {
+        if(this.root != null) {
+            postRec(this.root);
+        }
+    }
+
+    void inRec(Node root) {
+        if (root != null) {
+            inRec(root.left);
+            System.out.println(root.key.key);
+            inRec(root.right);
+        }
+    }
+
+    void preRec(Node root) {
+        System.out.println(root.key.key);
+        if(root.left != null) {
+            preRec(root.left);
+        }
+        if(root.right != null) {
+            preRec(root.right);
+        }
+    }
+
+    void postRec(Node root) {
+        if (root != null) {
+            postRec(root.left);
+            postRec(root.right);
+            System.out.println(root.key.key);
+        }
+    }
+
+    void printNodeComparisons() {
+        System.out.println(this.compares);
+
+    }
+}
+
+public class DN2 {
+    public static void main(String[] args) {
+        BST b = new BST();
+        b.insert(29);
+        b.insert(15);
+        b.insert(43);
+        b.insert(5);
+        b.insert(23);
+        b.insert(35);
+        b.insert(51);
+        b.insert(3);
+        b.insert(7);
+        b.insert(19);
+        b.insert(27);
+        b.insert(31);
+        b.insert(37);
+        b.insert(47);
+        b.insert(59);
+        b.delete(15);
+        b.delete(43);
+        b.printPreorder();
+        b.printPostorder();
+    }
+}
--- src/main.java
+++ src/main.java
@@ -3,16 +3,16 @@ import java.util.*;
 
 class ArrArray {
     public int stevec;
-    public ArrayList <int[]> table;
+    public ArrayList<int[]> table;
     HashMap<Integer, Integer> occ = new HashMap<>();
 
-    public ArrayList <Integer> temp = new ArrayList<>();
+    public ArrayList<Integer> temp = new ArrayList<>();
     public boolean test = false;
 
 
     public ArrArray() {
         table = new ArrayList<>();
-        int [] test = new int[1];
+        int[] test = new int[1];
         table.add(test);
         stevec = 0;
     }
@@ -23,22 +23,21 @@ class ArrArray {
         int contI = -1;
         int stevec_f = 0;
 
-        if(occ.containsKey(e)) {
+        if (occ.containsKey(e)) {
             int x = occ.get(e);
-            occ.put(e, x+1);
-        }
-        else{
+            occ.put(e, x + 1);
+        } else {
             occ.put(e, 1);
         }
 
-        if(find_two(e)) {
+        if (find_two(e)) {
             return;
         }
         temp.clear();
 
         for (int i = 0; i < table.size(); i++) {
             for (int j = 0; j < table.get(i).length; j++) {
-                if(table.get(i)[j] == -2) {
+                if (table.get(i)[j] == -2) {
                     cont = true;
                 }
             }
@@ -47,16 +46,16 @@ class ArrArray {
         temp.add(e);
 
         for (int i = 0; i < table.size(); i++) {
-            if(isEmpty(table.get(i))) {
+            if (isEmpty(table.get(i))) {
                 check = false;
-                if(temp.size() == table.get(i).length) {
+                if (temp.size() == table.get(i).length) {
                     for (int k = 0; k < temp.size(); k++) {
                         table.get(i)[k] = temp.get(k);
                     }
                     temp.clear();
 
-                    if(i > 0 && temp.size() > 0) {
-                        for (int k = i-1; k > 0 ; k--) {
+                    if (i > 0 && temp.size() > 0) {
+                        for (int k = i - 1; k > 0; k--) {
                             for (int l = 0; l < table.get(k).length; l++) {
                                 temp.add(table.get(l)[k]);
                             }
@@ -72,21 +71,20 @@ class ArrArray {
                     fix(stevec_f);
                     return;
                 }
-            }
-            else {
+            } else {
                 stevec_f++;
                 for (int j = 0; j < table.get(i).length; j++) {
                     if (table.get(i)[j] != 0) {
                         temp.add(table.get(i)[j]);
                     }
-                    if(table.get(i)[j] == -2) {
+                    if (table.get(i)[j] == -2) {
                         table.get(i)[j] = e;
                         temp.clear();
                         sorter(table.get(i));
                         return;
                     }
                 }
-                if(!cont) {
+                if (!cont) {
                     int[] nov = new int[(int) Math.pow(2, i)];
                     table.set(i, nov);
                     Collections.sort(temp);
@@ -94,11 +92,11 @@ class ArrArray {
             }
         }
 
-        if(check) {
+        if (check) {
             int[] nov = new int[(int) Math.pow(2, table.size())];
-            for (int i = table.size()-1; i >= 0; i--) {
+            for (int i = table.size() - 1; i >= 0; i--) {
                 for (int j = 0; j < table.get(i).length; j++) {
-                    if(table.get(i)[j] != 0) {
+                    if (table.get(i)[j] != 0) {
                         temp.add(table.get(i)[j]);
                     }
                 }
@@ -113,7 +111,7 @@ class ArrArray {
         }
     }
 
-    public void fix (int stevec) {
+    public void fix(int stevec) {
         for (int i = 0; i < stevec; i++) {
             int[] nov = new int[(int) Math.pow(2, i)];
             table.set(i, nov);
@@ -124,16 +122,15 @@ class ArrArray {
     public void find(int e) {
         boolean found = false;
         for (int i = 0; i < table.size(); i++) {
-            if(!isEmpty(table.get(i))) {
+            if (!isEmpty(table.get(i))) {
                 int min = table.get(i)[0];
-                int max = table.get(i)[table.get(i).length-1];
-                if(e == max || e == min) {
+                int max = table.get(i)[table.get(i).length - 1];
+                if (e == max || e == min) {
                     found = true;
-                }
-                else if(min < e && e < max) {
+                } else if (min < e && e < max) {
                     for (int j = 0; j < table.get(i).length; j++) {
-                        int x = binarySearch(table.get(i), 0, table.get(i).length-1, e);
-                        if(x != -1) {
+                        int x = binarySearch(table.get(i), 0, table.get(i).length - 1, e);
+                        if (x != -1) {
                             found = true;
                         }
                     }
@@ -145,17 +142,16 @@ class ArrArray {
 
     public boolean find_two(int e) {
         for (int i = 0; i < table.size(); i++) {
-            if(!isEmpty(table.get(i))) {
+            if (!isEmpty(table.get(i))) {
                 int min = table.get(i)[0];
-                int max = table.get(i)[table.get(i).length-1];
-                if(e == max || e == min) {
+                int max = table.get(i)[table.get(i).length - 1];
+                if (e == max || e == min) {
                     return true;
 
-                }
-                else if(min < e && e < max) {
+                } else if (min < e && e < max) {
                     for (int j = 0; j < table.get(i).length; j++) {
-                        int x = binarySearch(table.get(i), 0, table.get(i).length-1, e);
-                        if(x != -1) {
+                        int x = binarySearch(table.get(i), 0, table.get(i).length - 1, e);
+                        if (x != -1) {
                             return true;
                         }
                     }
@@ -168,44 +164,41 @@ class ArrArray {
 
     public void delete(int e) {
 
-        if(occ.containsKey(e)) {
+        if (occ.containsKey(e)) {
             int y = occ.get(e);
             occ.put(e, y - 1);
-        }
-        else {
+        } else {
             return;
         }
 
         boolean found = false;
         for (int i = 0; i < table.size(); i++) {
-            if(!isEmpty(table.get(i))) {
-                int [] mid = table.get(i);
+            if (!isEmpty(table.get(i))) {
+                int[] mid = table.get(i);
                 int min = table.get(i)[0];
-                int max = table.get(i)[table.get(i).length-1];
-                if(e == max) {
+                int max = table.get(i)[table.get(i).length - 1];
+                if (e == max) {
                     if (occ.get(e) == 0) {
                         mid[mid.length - 1] = -2;
                     }
                     found = true;
                 }
 
-                if(e == min) {
-                    if(occ.get(e) == 0) {
+                if (e == min) {
+                    if (occ.get(e) == 0) {
                         mid[0] = -2;
                     }
                     found = true;
-                }
-
-                else if(min < e && e < max) {
-                    int x = binarySearch(table.get(i), 0, table.get(i).length-1, e);
-                    if(x != -1) {
-                        if(occ.get(e) == 0) {
+                } else if (min < e && e < max) {
+                    int x = binarySearch(table.get(i), 0, table.get(i).length - 1, e);
+                    if (x != -1) {
+                        if (occ.get(e) == 0) {
                             mid[x] = -2;
                         }
                         found = true;
                     }
                 }
-                table.set(i,mid);
+                table.set(i, mid);
             }
         }
 
@@ -282,31 +275,29 @@ class ArrArray {
         boolean empty = true;
         for (int i = 0; i < table.size(); i++) {
             for (int j = 0; j < table.get(i).length; j++) {
-                if(table.get(i)[j] != -2 && table.get(i)[j] != 0) {
+                if (table.get(i)[j] != -2 && table.get(i)[j] != 0) {
                     empty = false;
                 }
             }
         }
-        if(table.size() == 1) {
+        if (table.size() == 1) {
             System.out.println("empty");
             return;
         }
-        if(empty) {
+        if (empty) {
             System.out.println("empty");
             return;
         }
         for (int i = 0; i < table.size(); i++) {
-            if(isEmpty(table.get(i))) {
-                System.out.println("A_"+ i + ": ...");
-            }
-            else {
+            if (isEmpty(table.get(i))) {
+                System.out.println("A_" + i + ": ...");
+            } else {
                 StringBuilder out = new StringBuilder();
-                out.append("A_"+ i + ": ");
+                out.append("A_" + i + ": ");
                 for (int j = 0; j < table.get(i).length; j++) {
-                    if(table.get(i)[j] == -2) {
+                    if (table.get(i)[j] == -2) {
                         out.append("x, ");
-                    }
-                    else {
+                    } else {
                         out.append(table.get(i)[j] + "/" + occ.get(table.get(i)[j]) + ", ");
                     }
                 }
@@ -319,15 +310,15 @@ class ArrArray {
 
     public boolean isEmpty(int[] arr) {
         boolean value = true;
-        for(int i = 0; i < arr.length; i++) {
-            if(arr[i] != 0) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
                 value = false;
             }
         }
         return value;
     }
 
-    public void sorter (int[] arr) {
+    public void sorter(int[] arr) {
         int n = arr.length;
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
@@ -339,8 +330,8 @@ class ArrArray {
             }
         }
     }
-    public int binarySearch(int arr[], int l, int r, int x)
-    {
+
+    public int binarySearch(int arr[], int l, int r, int x) {
         if (r >= l) {
             int mid = l + (r - l) / 2;
 
